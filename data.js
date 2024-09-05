edeecc6d6e39074eb7463fc7d21bff02334dd62d
Will Roger <rogeriosilva162@gmail.com>
2024-09-05 15:50:02 +0000

    vai carai

---
+++ data.js
@@ -0,0 +1,14 @@
+function getData() {
+  return [
+    { ingredient: "OTHER JOBS", weight: 6044.5 },
+    { ingredient: "PARKING AREA", weight: 6060 },
+    { ingredient: "TRAILER WASH", weight: 1460 },
+    { ingredient: "TRAILER UNSTACK", weight: 75 },
+    { ingredient: "DALIMARO", weight: 3064 },
+    { ingredient: "IWT", weight: 1533 },
+    { ingredient: "BLS", weight: 371 },
+    { ingredient: "SCHMIDT", weight: 1622.5 },
+    { ingredient: "REEFER", weight: 583 },
+    { ingredient: "QF", weight: 387 },
+  ];
+}
---
+++ main.js
@@ -0,0 +1,46 @@
+const { AgCharts } = agCharts;
+
+const numFormatter = new Intl.NumberFormat("en-US");
+
+const options = {
+  container: document.getElementById("myChart"),
+  title: {
+    text: "DEPOT REPORT",
+  },
+  subtitle: {
+    text: "August",
+  },
+  footnote: {
+    text: "INCOMES TOTAL: €21.200 ",
+  },
+  series: [
+    {
+      data: getData(),
+      type: "pie",
+      calloutLabelKey: "ingredient",
+      sectorLabelKey: "weight",
+      angleKey: "weight",
+      calloutLabel: {
+        offset: 10,
+      },
+      sectorLabel: {
+        formatter: ({ datum, sectorLabelKey = "weight" }) => {
+          return `${numFormatter.format(datum[sectorLabelKey])}€`;
+        },
+      },
+      tooltip: {
+        renderer: ({ datum, angleKey, calloutLabelKey = "ingredient" }) => ({
+          title: `${datum[calloutLabelKey]}`,
+          content: `${datum[angleKey]}g`,
+        }),
+      },
+      title: {
+        text: "",
+      },
+    },
+  ],
+  legend: {
+    enabled: false,
+  },
+};
+AgCharts.create(options);
